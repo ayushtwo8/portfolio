9ece154f5dbfbe5d12c49a43d6ef00f0732aa6c8
Ayush Tiwari <ayushh8866@gmail.com>
2025-07-12 21:54:37 +0530

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,13 +23,14 @@ const Navbar = () => {
     }, 1000);
     return () => clearInterval(interval);
   }, []);
+
   return (
-    <div className="h-16 px-6 ">
-      <div className="flex justify-between items-center h-full mx-8">
-        <div className="text-white/90 font-bold text-2xl font-serif">
+    <div className="h-14 sm:h-16 px-4 sm:px-6">
+      <div className="flex justify-between items-center h-full mx-2 sm:mx-4 lg:mx-8">
+        <div className="text-white/90 font-bold text-lg sm:text-xl lg:text-2xl font-serif">
           <a href="#">{time}</a>
         </div>
-        <div className="flex gap-16 text-white/70">
+        <div className="flex gap-6 sm:gap-8 lg:gap-16 text-white/70 text-sm sm:text-base">
           <a
             href="#experience"
             className="hover:text-white transition cursor-pointer"
@@ -42,7 +43,6 @@ const Navbar = () => {
           >
             Projects
           </a>
-          
         </div>
       </div>
     </div>
